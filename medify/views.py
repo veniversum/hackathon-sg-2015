16c0c1be5df6813f753b1a1f0f07f048c147bb93
Chua Eu Jing <chuaeujing@gmail.com>
2015-07-26 01:56:07 +0800

    Checked for duplicates in matching

--- medify/views.py
+++ medify/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from . import models
+
+import subdist
 # Create your views here.
 
 
@@ -45,21 +47,33 @@ def omni_search(request):
 
         illegalMeds = models.IllegalMedication.objects.all()
         approvedMeds = models.ApprovedMedication.objects.all()
+        approvedDevices = models.ApprovedDevice.objects.all()
+
         results = {
             "illegal_medication": [],
-            "approved_medication": []
+            "approved_medication": [],
+            "approved_devices": [],
         }
 
         illegalMedValues = illegalMeds.values()
         for value_name in ["product_name", "manufacturer"]:
             for med in illegalMedValues:
-                if fuzzy_substring(substring.lower(), med[value_name].lower()) < 0.5 * len(substring):
-                    results["illegal_medication"].append(med)
+                if fuzzy_substring(substring.lower(), med[value_name].lower()) < 0.25 * len(substring):
+                    if med not in results["illegal_medication"]:
+                        results["illegal_medication"].append(med)
 
         approvedMedValues = approvedMeds.values()
         for value_name in ["product_name", "manufacturer", "active_ingredients"]:
             for med in approvedMedValues:
                 if fuzzy_substring(substring.lower(), med[value_name].lower()) < 0.25 * len(substring):
-                    results["approved_medication"].append(med)
+                    if med not in results["approved_medication"]:
+                        results["approved_medication"].append(med)
+
+        approvedDeviceValues = approvedDevices.values()
+        for value_name in ["device_name", "product_owner_name", "models_name"]:
+            for dev in approvedDeviceValues:
+                if fuzzy_substring(substring.lower(), dev[value_name].lower()) < 0.25 * len(substring):
+                    if dev not in results["approved_devices"]:
+                        results["approved_devices"].append(dev)
 
         return JsonResponse(results)
