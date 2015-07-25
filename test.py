1f056bdee6f6f23f65600a9263db0e9151a62833
veniversum <hello@veniversum.me>
2015-07-25 19:47:14 +0800

    Refactor medicine data file

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import json
-from medify.models import ApprovedMedication, IllegalMedication
-data_file = open('data\medicinal2.json')
+from medify.models import ApprovedMedication 
+data_file = open('data\medicinal.json')
 data = json.load(data_file)
 
 
@@ -9,7 +9,7 @@ for item in ls:
 	item["approval_date"] = item["approval_date"][-4:]+"-"+item["approval_date"][3:5]+"-"+item["approval_date"][:2]
 	med= ApprovedMedication(**item)
 	med.save()
-
+	
 
 data_file = open('data\illegal.json')
 data = json.load(data_file)
